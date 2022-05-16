fe928536e089ba92626edf32daeb079333888824
Vincent Nuku <vincent.nuku@hotmail.co.nz>
2022-05-16 13:51:14 +1200

    Completed inflate-the-unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,21 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+
+document.getElementById("uni0").onclick = clicked
+document.getElementById("uni1").onclick = clicked
+document.getElementById("uni2").onclick = clicked
+
+let inflation = [0, 0, 0]
+
+function clicked(event)  {
+  let unicorn = event.target
+  let id = unicorn.id[3]
+
+  inflation[id]++
+
+  if (inflation[id] == 4) 
+      inflation[id] = 0
+      
+  unicorn.src = "./images/unicorn-" + inflation[id] + ".png"
+}
